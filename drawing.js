e384faac4457faf42d8be324cd19e057a2061477
Yann Zaoui <yzaoui7@gmail.com>
2018-04-01 18:49:57 -0400

    Refactor into separate files

---
+++ drawing.js
@@ -0,0 +1,37 @@
+function drawGrid(ctx, minor_, major_, stroke_, fill_) {
+    let minor = minor_ || 10;
+    let major = major_ || minor * 5;
+    let stroke = stroke_ || "#00FF00";
+    let fill = fill_ || "#009900";
+
+    ctx.save();
+
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    let width = ctx.canvas.width;
+    let height = ctx.canvas.height;
+
+    for (let x = 0; x < width; x += minor) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.lineWidth = (x % major === 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        if (x % major === 0) {
+            ctx.fillText(x.toString(), x, 10);
+        }
+    }
+
+    for (let y = 0; y < height; y += minor) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.lineWidth = (y % major === 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        if (y % major === 0) {
+            ctx.fillText(y.toString(), 0, y + 10);
+        }
+    }
+
+    ctx.restore();
+}
